af510a9570fe1178ffc68f635de865a23187c588
yvesen <l.sen.t@hotmail.com>
2018-05-28 22:03:37 +0800

    add db:migrate

--- db/migrate/20180524150313_create_shoppings.rb
+++ db/migrate/20180524150313_create_shoppings.rb
@@ -1,7 +1,9 @@
 class CreateShoppings < ActiveRecord::Migration[5.1]
   def change
     create_table :shoppings do |t|
-
+      t.string :name
+      t.text :description
+      t.string :price
       t.timestamps
     end
   end
