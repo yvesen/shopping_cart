28a5672a8cc8937f153b6b399087e8f0c0fe5967
yvesen <l.sen.t@hotmail.com>
2018-05-29 16:26:16 +0800

    add column image to shoppings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180528142452) do
+ActiveRecord::Schema.define(version: 20180529082332) do
 
   create_table "shoppings", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180528142452) do
     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
