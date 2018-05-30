57ad77338717131c8f27150a9616ca5d3f805779
yvesen <l.sen.t@hotmail.com>
2018-05-30 13:34:17 +0800

    shopping resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "shoppings#index"
 
+  resources :shoppings, only: [:index, :show]
+
   namespace :admin do
   	resources :shoppings
     root "shoppings#index"
