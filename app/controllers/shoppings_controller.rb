cd90618936dd95da90b99ffdd520641cc1e65ad8
yvesen <l.sen.t@hotmail.com>
2018-05-31 10:46:20 +0800

    add public show controller and admin show page div

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,8 +1,12 @@
 class ShoppingsController < ApplicationController
+	before_action :set_shopping, only: [:show]
 
 
   def index 
   	@shoppings = Shopping.page(params[:page]).per(12)
+  end
+
+  def show
   end
 
   def add_to_cart
@@ -12,5 +16,14 @@ class ShoppingsController < ApplicationController
     redirect_to root_path
   end
 
+  private
+
+  def shopping_params
+    params.require(:shopping).permit(:name, :description, :price, :image)
+  end
+
+  def set_shopping
+  	@shopping = Shopping.find(params[:id])
+  end
 
 end
