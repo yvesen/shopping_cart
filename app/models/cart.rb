024a7e0ed55508feefa76e669c86fc7658126077
yvesen <l.sen.t@hotmail.com>
2018-06-01 10:39:43 +0800

    index cart success

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-	has_many :cart_item, dependent: :destroy
+	has_many :cart_items, dependent: :destroy
   has_many :shoppings, through: :cart_items
 
 	def add_cart_item(shopping)
@@ -8,7 +8,7 @@ class Cart < ApplicationRecord
       existing_item.quantity += 1
       existing_item.save!
     else
-      cart_item = cart_items.build( shopping_id: shopping )
+      cart_item = cart_items.build( shopping_id: shopping.id )
       cart_item.save!
     end
     self.cart_items
--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -2,5 +2,5 @@ class Shopping < ApplicationRecord
 	validates_presence_of :name
 	mount_uploader :image, PhotoUploader
 	has_many :cart_items, dependent: :destroy
-  has_many :carts, through: :cart_items
+  has_many :cart, through: :cart_items
 end
